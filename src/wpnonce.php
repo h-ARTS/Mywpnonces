fcd6f09f61147a8516fae1c2224c66d837cd1ed1
Hanan Mufti <hi5hanan@yahoo.com>
2017-03-28 11:41:47 +0200

    wpnonces edited, wpnoncetest & wp_mock

--- src/wpnonce.php
+++ src/wpnonce.php
@@ -2,9 +2,40 @@
 
 namespace src;
 
-class MYWPNonce {
+class MYWPNonce 
+{
 
      private $action;
+
+     public function __contruct($action) {
+          $this->action = $action;
+     }
+
+     public function get_the_action() {
+
+          return $this->action;
+
+     }
+
+     public function createNonce() {
+         
+          if(!function_exists('wp_create_nonce()')) { return false; }
+
+          return wp_create_nonce($this->action);
+
+     }
+
+     public function retriveNonceField() {
+
+          if(!function_exists('wp_nonce_field()')) { return false; }
+
+          $name = '_wpnonce';
+          $referer = true;
+          $echo = true;
+
+          return wp_nonce_field( $this->action, $name, $referer, $echo );
+
+     }
 
 }
 
--- tests/wpnoncetest.php
+++ tests/wpnoncetest.php
@@ -4,6 +4,15 @@ use PHPUnit\Framework\TestCase;
 
 class MYWPNonceTest extends \PHPUnit_Framework_TestCase
 {
+
+     public function setUp() {
+          \WP_Mock::setUp();
+     }
+
+     public function tearDown() {
+          \WP_Mock::tearDown();
+     }
+
      public function testReturnsTrue() {
           
           $this->assertTrue(true);
